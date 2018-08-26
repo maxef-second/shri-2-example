f7d1e4b4a6bcd4235d31a606404a14b81d9fed28
maxef23 <max_efimovskiy@mail.ru>
2018-08-26 05:43:25 +0300

    .

---
+++ index.js
@@ -0,0 +1,108 @@
+// 2 слайдер
+$('.devices__blocks__list')[0].style.left = "0";
+
+$('.devices__header__nav__right_arrow').on('click', function(e) {
+	if ($(window).width()*0.975 <= (parseInt($('.devices__blocks__list').width()) + parseInt($('.devices__blocks__list')[0].style.left))){
+		$('.devices__blocks__list').animate({left: parseInt($('.devices__blocks__list')[0].style.left)-215}, 0);
+	}
+});
+$('.devices__header__nav__left_arrow').on('click', function(e) {
+	if (!(parseInt($('.devices__blocks__list')[0].style.left) == 0)){
+		$('.devices__blocks__list').animate({left: parseInt($('.devices__blocks__list')[0].style.left)+215}, 0);
+	}
+});
+
+
+
+// 1 слайдер
+$('.script__blocks__list__horizontal')[0].style.left = "0";
+
+var currentScriptSlide = 1;
+
+
+$('.script__header__nav__right_arrow').on('click', function(e) {
+	if (currentScriptSlide != $('.script__blocks__list__horizontal').children().length){
+		currentScriptSlide++;
+		$('.script__blocks__list__horizontal').animate({left: parseInt($('.script__blocks__list__horizontal')[0].style.left)-$('.script__blocks__list__horizontal').width()/$('.script__blocks__list__horizontal').children().length}, 0);
+	}
+});
+$('.script__header__nav__left_arrow').on('click', function(e) {
+	if (currentScriptSlide != 1){
+		currentScriptSlide--;
+		$('.script__blocks__list__horizontal').animate({left: parseInt($('.script__blocks__list__horizontal')[0].style.left)+$('.script__blocks__list__horizontal').width()/$('.script__blocks__list__horizontal').children().length}, 0);
+	}
+});
+
+
+if ($('.devices__blocks__list').width() < $('body').width()){
+	$('.devices__header__nav').hide();
+}
+
+if ($('.script__blocks__list').width() < $('body').width()*0.4){
+	$('.script__header__nav').hide();
+}
+
+
+
+
+// main__blocks__scripts__scroll
+var main__block__scripts__itemTemplate = '<div class="main__block__scripts__item__next"><img width=16px height=16px src="img/Icons/%20Arrow%20Double%20/%20M.png"></div>'
+
+var array_template = [];
+for (var i = 0; i < $('.main__block__scripts__item').length; i++){
+	array_template.push($($('.main__block__scripts__item')[i])[0].innerHTML);
+}
+
+if ($(window).width() > '1250'){
+	for (i = 0; i < $('.main__block__scripts__item').length; i++){
+			var main__block__scripts__item = array_template[i];
+		if ($($('.main__block__scripts__item')[i]).offset().top < 150 + $(".main__block__scripts").height() - 50) {
+			$($('.main__block__scripts__item')[i])[0].innerHTML = main__block__scripts__item;
+		} else {
+			$($('.main__block__scripts__item')[i])[0].innerHTML = main__block__scripts__itemTemplate;
+		}
+	}
+}
+
+
+$(".main__block__scripts").scroll(function() {
+	if ($(window).width() > '1250'){
+		for (i = 0; i < $('.main__block__scripts__item').length; i++){
+			var main__block__scripts__item = array_template[i];
+		if ($($('.main__block__scripts__item')[i]).offset().top < 150 + $(".main__block__scripts").height() - 50) {
+			$($('.main__block__scripts__item')[i])[0].innerHTML = main__block__scripts__item;
+		} else {
+			$($('.main__block__scripts__item')[i])[0].innerHTML = main__block__scripts__itemTemplate;
+		}
+	}
+}
+});
+
+
+
+var label__mobile = 1;
+
+	$(window).resize(function(){
+		
+	 	if($(window).width() > 840){
+	 		$(".script__blocks__list__horizontal").width($(".script__blocks__list__horizontal").children().length*100 + "%");
+	 	} else {
+	 		for (var i = 0; i < $(".script__blocks__list__horizontal").children().length; i++){
+	 		  $($('.script__blocks__list')[i]).css("min-width", $($('.script__blocks__list')[i]).children().length*215 + "px");
+	 		  if (i == $(".script__blocks__list__horizontal").children().length - 1){
+	 		  	$($('.script__blocks__list')[i]).css("min-width", $($('.script__blocks__list')[i]).width()-15);
+	 		  }
+			}
+	 	}
+	 	if ($(window).width() < 1250){
+	 		for (var i = 0; i < $('.main__block__scripts__item').length; i++){
+				$($('.main__block__scripts__item')[i])[0].innerHTML = array_template[i];
+			}
+	 	}
+	 	if ($(window).width() < 840){
+	 		$('.xiaomi_yeelight_led_smart_bulb__progress__bar').width($(".xiaomi_yeelight_led_smart_bulb").height()*0.5)
+	 	}
+	});
+
+	window.onresize = $(window).resize();
+
